07c39f52be387ed8702a33c3a5fb60e82c438722
Sébastien Gonzalez <>
2019-04-21 17:51:34 +0200

    Ajout affichage de la map

---
+++ geolocalisation/src/main/java/fr/ufr/science/geolocalisation/MainWindow.java
@@ -0,0 +1,58 @@
+package fr.ufr.science.geolocalisation;
+
+import java.io.File;
+
+import javax.swing.JFrame;
+import javax.swing.event.MouseInputListener;
+
+import org.jxmapviewer.JXMapViewer;
+import org.jxmapviewer.OSMTileFactoryInfo;
+import org.jxmapviewer.cache.FileBasedLocalCache;
+import org.jxmapviewer.input.CenterMapListener;
+import org.jxmapviewer.input.PanKeyListener;
+import org.jxmapviewer.input.PanMouseInputListener;
+import org.jxmapviewer.input.ZoomMouseWheelListenerCursor;
+import org.jxmapviewer.viewer.DefaultTileFactory;
+import org.jxmapviewer.viewer.GeoPosition;
+import org.jxmapviewer.viewer.TileFactoryInfo;
+
+public class MainWindow extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+
+	public MainWindow() {
+		// Create a TileFactoryInfo for OpenStreetMap
+        TileFactoryInfo info = new OSMTileFactoryInfo();
+        DefaultTileFactory tileFactory = new DefaultTileFactory(info);
+
+        // Setup local file cache
+        File cacheDir = new File(System.getProperty("user.home") + File.separator + ".jxmapviewer2");
+        tileFactory.setLocalCache(new FileBasedLocalCache(cacheDir, false));
+
+        // Setup JXMapViewer
+        final JXMapViewer mapViewer = new JXMapViewer();
+        mapViewer.setTileFactory(tileFactory);
+
+        //toni ?
+        GeoPosition france = new GeoPosition(46.227638, 2.213749);
+
+        // Set the focus
+        mapViewer.setZoom(13);
+        mapViewer.setAddressLocation(france);
+
+        // Add interactions
+        MouseInputListener mia = new PanMouseInputListener(mapViewer);
+        mapViewer.addMouseListener(mia);
+        mapViewer.addMouseMotionListener(mia);
+
+        mapViewer.addMouseListener(new CenterMapListener(mapViewer));
+
+        mapViewer.addMouseWheelListener(new ZoomMouseWheelListenerCursor(mapViewer));
+
+        mapViewer.addKeyListener(new PanKeyListener(mapViewer));
+        
+        this.setSize(800, 600);
+        this.add(mapViewer);
+        this.setVisible(true);
+	}
+}
