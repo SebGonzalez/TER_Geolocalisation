1908abd9d74f81ccede84d7aebe5322618822452
ppp94 <pierre.sbroggio@etu.univ-amu.fr>
2019-04-22 11:07:38 +0200

    Fix bad repo

--- geolocalisation/src/main/java/fr/ufr/science/geolocalisation/Personne.java
+++ geolocalisation/src/main/java/fr/ufr/science/geolocalisation/Personne.java
@@ -1,5 +1,78 @@
 package fr.ufr.science.geolocalisation;
 
-public class Personne {
+import java.util.ArrayList;
 
+public class Personne 
+{
+	private long numClient;
+	
+	private String Nom;
+	private String Prenom;
+	
+	private String ville;
+	private String pays;
+	
+	ArrayList<String> infoComplementaireNom;
+	ArrayList<String> infoComplementaire;
+
+	public long getNumClient() {
+		return numClient;
+	}
+
+	public void setNumClient(long numClient) {
+		this.numClient = numClient;
+	}
+
+	public String getNom() {
+		return Nom;
+	}
+
+	public void setNom(String nom) {
+		Nom = nom;
+	}
+
+	public String getPrenom() {
+		return Prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		Prenom = prenom;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getPays() {
+		return pays;
+	}
+
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+
+	public ArrayList<String> getInfoComplementaireNom() {
+		return infoComplementaireNom;
+	}
+
+	public void setInfoComplementaireNom(ArrayList<String> infoComplementaireNom) {
+		this.infoComplementaireNom = infoComplementaireNom;
+	}
+
+	public ArrayList<String> getInfoComplementaire() {
+		return infoComplementaire;
+	}
+
+	public void setInfoComplementaire(ArrayList<String> infoComplementaire) {
+		this.infoComplementaire = infoComplementaire;
+	}
+
+	
+
+	
+	
 }
